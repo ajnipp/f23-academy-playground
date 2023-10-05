7753c2efc843635897cf9e5eed3f372fb008ef55
A-Nipp <65468669+ajnipp@users.noreply.github.com>
2023-10-05 17:28:48 -0400

    Initial Commit

---
+++ f23-academy-playground/f23_academy_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  f23_academy_playgroundApp.swift
+//  f23-academy-playground
+//
+//  Created by AlecNipp on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct f23_academy_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
